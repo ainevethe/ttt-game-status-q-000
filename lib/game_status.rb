294b637feb76723608d163ceee892945930432dd
Crista Cooper <aine3d@gmail.com>
2017-10-15 20:34:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -72,9 +72,9 @@ def winner(board)
 
   WIN_COMBINATIONS.find do |win_combination|
 
-    if (board[win_combination[0] == "X" && board[win_combination[1] == "X" && board[win_combination[2] == "X")
+    if ((board[win_combination[0] == "X") && (board[win_combination[1] == "X") && (board[win_combination[2] == "X"))
       return "X"
-    elsif (board[win_combination[0] == "O" && board[win_combination[1] == "O" && board[win_combination[2] == "O")
+    elsif ((board[win_combination[0] == "O") && (board[win_combination[1] == "O") && (board[win_combination[2] == "O"))
       return "O"
     else
       return nil
