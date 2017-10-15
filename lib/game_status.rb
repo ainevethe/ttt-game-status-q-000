6cdaee6eb83fb2c3aa55f07cb5ea187521654389
Crista Cooper <aine3d@gmail.com>
2017-10-15 19:02:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,57 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+
+    [0,1,2], #top row
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+
+def won?(board)
+
+  win_combo = true
+
+  WIN_COMBINATIONS.each do |win_combination|
+
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O"))
+       return win_combination.to_ary
+     else
+       win_combo = false
+     end
+   end
+   return win_combo
+end
+
+def full?(board)
+  board.each do |spot|
+    cell1 = spot[0]
+    cell2 = spot[1]
+    cell3 = spot[2]
+
+    position1 = board[cell1]
+    position2 = board[cell2]
+    position3 = board[cell3]
+
+    board.each do i |cells|
+      if cells[i] = "" || " "
+        return false
+      else
+        return true
+      end
+    end
+end
